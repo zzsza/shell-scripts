7f6757ef8cca2e8098909c535fe95ffb0b800440
zzsza <zzsza@naver.com>
2019-06-17 00:09:52 +0900

    [update] random pick script

---
+++ random_pick.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+def search(dirname, extension='.sh'):
+	filenames = os.listdir(dirname)
+	result_lists = []
+	for filename in filenames:
+		ext = os.path.splitext(filename)[-1]
+		if ext == extension:
+			result_lists.append(filename)
+	return result_lists
+
+current_folder = os.getcwd()
+script_folder = current_folder + '/scripts/'
+script_lists = search(script_folder)
+print("문제를 풀어보세요 : ", np.random.choice(script_lists, 1))
