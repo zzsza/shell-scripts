377e0dfd5e8bc17ec242cc2ab49af18d8046dccf
zzsza <zzsza@naver.com>
2019-06-19 00:33:45 +0900

    [update] 설명

--- scripts/009_interactive_interface_in_linux_and_mac.sh
+++ scripts/009_interactive_interface_in_linux_and_mac.sh
@@ -22,7 +22,16 @@ LOG_DIR=/myapp/ap1/log
 
 # Mac은 dialog가 없고 osascript가 존재
 osascript -e 'tell app "iTerm" to display dialog "Hello! Popup" with title "script" buttons {"yes","no"}' 2 >return.tmp
+# tell app "앱 이름" : 앱에서
+# to display dialog "팝업 내용" : 팝업 내용으로 창을 띄우고
+# with title "타이틀 이름" : 타이틀 이름의 제목을 가지고
+# buttons {"yes", "no"} : yes와 no 2개 버튼을 가진 것을 만들어라!
+# 결과는 return.tmp에 저장해라
 
 return_value=$(awk -F / '{split($1, arr, ":"); print arr[2]}' return.tmp)
+# awk -F : 입력 필드를 나눌 때 $1 $2로 사용할 수 있음 
+# split($1, arr, ":") : $1 값을 ":"로 split하고 그 결과를 arr에 넣어라
+# 맨 뒤에 return.tmp는 이걸 읽어서
+
 echo "$return_value"
 rm -f return.tmp
